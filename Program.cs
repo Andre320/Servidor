90d70b61eeb4da2d385ecdf1075045647603ce48
Andre Salazar <andresalaeli.13@gmail.com>
2021-06-11 17:24:56 -0600

    First commit!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Servidor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Buen compi el Kea!");
         }
     }
 }
